e61df712240ee6f4d1ecd8b805b0886b59b85925
viswavi <vijayv@allenai.org>
2021-06-18 18:23:47 +0000

    Update model trainer and dataloader to support the Drug Synergy dataset

--- train.py
+++ train.py
@@ -1,17 +1,23 @@
 import argparse
 import jsonlines
 import pytorch_lightning as pl
-
-from data_loader import DrugSynergyDataModule
-from model import BertForRelation, RelationExtractor
-from preprocess import create_dataset
-
+from transformers import AutoTokenizer
 from transformers.file_utils import PYTORCH_PRETRAINED_BERT_CACHE
 
+from constants import ENTITY_END_MARKER, ENTITY_START_MARKER
+from data_loader import DrugSynergyDataModule
+from model import BertForRelation, RelationExtractor
+from preprocess import create_dataset, LABEL2IDX
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--pretrained-lm', type=str, required=False, default="microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract", help="path to Huggingface Transformers model path")
-parser.add_argument('--training-file', type=str, required=False, default="data/examples2.jsonl")
-parser.add_argument('--test-file', type=str, required=False, default="data/examples.jsonl")
+parser.add_argument('--pretrained-lm', type=str, required=False, default="microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract", help="Path to pretrained Huggingface Transformers model")
+parser.add_argument('--training-file', type=str, required=False, default="examples2_fixed.jsonl")
+parser.add_argument('--test-file', type=str, required=False, default="examples2_fixed.jsonl")
+parser.add_argument('--batch-size', type=int, required=False, default=12) # This number is good for training on an 11GB Tesla K80 GPU.
+parser.add_argument('--dev-train-split', type=float, required=False, default=0.1, help="Fraction of the training set to hold out for validation")
+parser.add_argument('--max-seq-length', type=int, required=False, default=512, help="Maximum subword length of the document passed to the encoder, including inserted marker tokens")
+parser.add_argument('--preserve-case', action='store_true')
+parser.add_argument("--num-train-epochs", default=3, type=int, help="Total number of training epochs to perform.")
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -21,26 +27,28 @@ if __name__ == "__main__":
     training_data = create_dataset(training_data)
     test_data = create_dataset(test_data)
 
-    data_module = DrugSynergyDataModule(training_data, test_data)
-    data_module.setup()
+    tokenizer = AutoTokenizer.from_pretrained(args.pretrained_lm, do_lower_case=not args.preserve_case)
+    tokenizer.add_tokens([ENTITY_START_MARKER, ENTITY_END_MARKER])
+    dm = DrugSynergyDataModule(training_data,
+                                        test_data,
+                                        tokenizer,
+                                        LABEL2IDX,
+                                        train_batch_size=args.batch_size,
+                                        dev_batch_size=args.batch_size,
+                                        test_batch_size=args.batch_size,
+                                        dev_train_ratio=args.dev_train_split,
+                                        max_seq_length=args.max_seq_length)
+    dm.setup()
 
-    num_labels=None
-    num_labels=4
-    assert num_labels is not None, "Not Implemented"
-
+    num_labels=len(set(dm.label_to_idx.values()))
     model = BertForRelation.from_pretrained(
             args.pretrained_lm, cache_dir=str(PYTORCH_PRETRAINED_BERT_CACHE), num_rel_labels=num_labels)
-
-    marker=2
-    breakpoint()
-    num_train_optimization_steps = None
-    assert num_train_optimization_steps is not None, "Not Implemented"
-    system = RelationExtractor(model=model, )
-
+    num_train_optimization_steps = len(dm.train_dataloader()) * float(args.num_train_epochs)
+    system = RelationExtractor(model, num_train_optimization_steps)
     trainer = pl.Trainer(
         gpus=1,
         precision=16,
-        max_epochs=3,
+        max_epochs=args.num_train_epochs,
     )
-
-    trainer.fit(model, data_module)
+    trainer.fit(system, datamodule=dm)
+    trainer.test(system, datamodule=dm)
