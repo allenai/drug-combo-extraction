6f33c1c40893ed20e3d3ef199557a5f260306e38
viswavi <vijayv@allenai.org>
2021-06-21 03:55:20 +0000

    Fix bugs in f1 evaluation

--- utils.py
+++ utils.py
@@ -4,18 +4,29 @@ def accuracy(predictions, labels):
     acc = float(torch.sum(predictions == labels).item()) / len(predictions)
     return acc
 
+def true_positives(predictions, labels):
+    tps = float(torch.sum(torch.logical_and(predictions == 1.0, labels == 1.0)).item())
+    return tps
+
 def precision(predictions, labels):
-    predicted_positives = float(torch.sum(predictions))
-    true_positives = float(torch.sum(predictions == labels).item())
-    return true_positives/predicted_positives
+    predicted_pos = float(torch.sum(predictions))
+    true_pos = true_positives(predictions, labels)
+    if true_pos == 0.0:
+        return 0.0
+    return true_pos/predicted_pos
 
 def recall(predictions, labels):
-    gt_positives = float(torch.sum(labels))
-    true_positives = float(torch.sum(predictions == labels).item())
-    return true_positives/gt_positives
+    gt_pos = float(torch.sum(labels))
+    true_pos = true_positives(predictions, labels)
+    if gt_pos == 0.0:
+        return 0.0
+    return true_pos/gt_pos
 
 def f1(predictions, labels):
     prec = precision(predictions, labels)
     rec = recall(predictions, labels)
-    f1 = 2*((prec*rec)/(prec+rec))
-    return f1, prec, rec
+    if prec+rec == 0.0:
+        f = 0.0
+    else:
+        f = 2*((prec*rec)/(prec+rec))
+    return f, prec, rec
