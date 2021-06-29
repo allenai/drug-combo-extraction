56808ec1e1ba6653c87b447a400759b7db135db5
Vijay Viswanathan <vcomeinpeace@gmail.com>
2021-06-29 02:44:09 +0000

    Add create_dataset option to upsample or downsample points from each class

--- preprocess.py
+++ preprocess.py
@@ -87,6 +87,7 @@ def process_doc(raw: Dict, add_no_combination_relations: bool = True) -> Documen
     Args:
         raw: Document from the Drug Synergy dataset, corresponding to one annotated sentence.
         add_no_combination_relations: Whether to add implicit NOT-COMB relations.
+        include_paragraph_context: Whether to include full-paragraph context around each drug-mention sentence
 
     Returns:
         document: Processed version of the input document.
@@ -167,13 +168,15 @@ def create_datapoints(raw: Dict, mark_entities: bool = True):
         samples.append({"text": text, "target": relation.relation_label})
     return samples
 
-def create_dataset(raw_data: List[Dict], shuffle: bool = True) -> List[Dict]:
+def create_dataset(raw_data: List[Dict], shuffle: bool = True, sample_negatives_ratio=1.0, sample_positives_ratio=1.0) -> List[Dict]:
     """Given the raw Drug Synergy dataset (directly read from JSON), convert it to a list of pairs
     consisting of marked text and a relation label, for each candidate relation in each document.
 
     Args:
         raw_data: List of documents in the dataset.
         shuffle: Whether or not to randomly reorder the relation instances in the dataset before returning.
+        sample_negatives_ratio: Ratio at which to sample negatives, to mitigate label imbalance.
+        sample_positives_ratio: Ratio at which to sample positives, to mitigate label imbalance.
 
     Returns:
         dataset: A list of text, label pairs (represented as a dictionary), ready to be consumed by a model.
@@ -182,6 +185,12 @@ def create_dataset(raw_data: List[Dict], shuffle: bool = True) -> List[Dict]:
     for row in raw_data:
         datapoints = create_datapoints(row)
         dataset.extend(datapoints)
+    if sample_negatives_ratio != 1.0 or sample_positives_ratio != 1.0:
+        non_negatives = [d for d in dataset if d["target"] != 0]
+        negatives = [d for d in dataset if d["target"] == 0]
+        non_negatives = random.choices(non_negatives, k=int(len(non_negatives) * sample_positives_ratio))
+        negatives = random.choices(negatives, k=int(len(negatives) * sample_negatives_ratio))
+        dataset = non_negatives + negatives
     if shuffle:
         random.shuffle(dataset)
     return dataset
