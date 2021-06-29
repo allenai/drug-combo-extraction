798a097a7d4018e42c1b64e86a9fcceba55f7184
Vijay Viswanathan <vcomeinpeace@gmail.com>
2021-06-29 02:42:59 +0000

    Add data upsampling argmuent to training script

--- train.py
+++ train.py
@@ -17,27 +17,29 @@ parser.add_argument('--batch-size', type=int, required=False, default=12) # This
 parser.add_argument('--dev-train-split', type=float, required=False, default=0.1, help="Fraction of the training set to hold out for validation")
 parser.add_argument('--max-seq-length', type=int, required=False, default=512, help="Maximum subword length of the document passed to the encoder, including inserted marker tokens")
 parser.add_argument('--preserve-case', action='store_true')
-parser.add_argument("--num-train-epochs", default=3, type=int, help="Total number of training epochs to perform.")
+parser.add_argument('--num-train-epochs', default=3, type=int, help="Total number of training epochs to perform.")
+parser.add_argument('--negative-sampling-rate', default=1.0, type=float, help="Upsample or downsample negative training examples for training (due to label imbalance)")
+parser.add_argument('--positive-sampling-rate', default=25.0, type=float, help="Upsample or downsample positive training examples for training (due to label imbalance)")
 
 if __name__ == "__main__":
     args = parser.parse_args()
 
     training_data = list(jsonlines.open(args.training_file))
     test_data = list(jsonlines.open(args.test_file))
-    training_data = create_dataset(training_data)
+    training_data = create_dataset(training_data, sample_negatives_ratio=args.negative_sampling_rate, sample_positives_ratio=args.positive_sampling_rate)
     test_data = create_dataset(test_data)
 
     tokenizer = AutoTokenizer.from_pretrained(args.pretrained_lm, do_lower_case=not args.preserve_case)
     tokenizer.add_tokens([ENTITY_START_MARKER, ENTITY_END_MARKER])
     dm = DrugSynergyDataModule(training_data,
-                                        test_data,
-                                        tokenizer,
-                                        LABEL2IDX,
-                                        train_batch_size=args.batch_size,
-                                        dev_batch_size=args.batch_size,
-                                        test_batch_size=args.batch_size,
-                                        dev_train_ratio=args.dev_train_split,
-                                        max_seq_length=args.max_seq_length)
+                            test_data,
+                            tokenizer,
+                            LABEL2IDX,
+                            train_batch_size=args.batch_size,
+                            dev_batch_size=args.batch_size,
+                            test_batch_size=args.batch_size,
+                            dev_train_ratio=args.dev_train_split,
+                            max_seq_length=args.max_seq_length)
     dm.setup()
 
     num_labels=len(set(dm.label_to_idx.values()))
