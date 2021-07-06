4996483b60f1c200a496e2a468afb5b69730e593
Vijay Viswanathan <vcomeinpeace@gmail.com>
2021-07-06 15:44:57 +0000

    Use F1 metric implementation from the PURE repository

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ from typing import Callable, List, Optional
 
 from constants import ENTITY_PAD_IDX
 from optimizers import adamw_with_linear_warmup, simple_adamw
-from utils import accuracy, f1
+from utils import accuracy, compute_f1, f1
 
 BertLayerNorm = torch.nn.LayerNorm
 
@@ -207,7 +207,8 @@ class RelationExtractor(pl.LightningModule):
         self.test_predictions.extend(predictions.tolist())
         self.test_batch_idxs.extend([batch_idx for _ in predictions.tolist()])
         acc = accuracy(predictions, labels)
-        f, prec, rec = f1(predictions, labels)
+        metrics_dict = compute_f1(predictions, labels)
+        f, prec, rec = metrics_dict["f1"], metrics_dict["precision"], metrics_dict["recall"]
         self.log("accuracy", acc, prog_bar=True, logger=True)
         self.log("precision", prec, prog_bar=True, logger=True)
         self.log("recall", rec, prog_bar=True, logger=True)
