546beb145e9911b6345dd9e28a46ce2e6295245c
aryeht <aryehgigi@gmail.com>
2021-08-17 16:07:14 +0300

    test exact match

--- eval.sh
+++ eval.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 python produce_gold_jsonl.py "$1" temp_out.jsonl
-python eval.py --gold-file temp_out.jsonl --pred-file "$2"
+if [ $# -eq 3 ] ; then
+  python eval.py --gold-file temp_out.jsonl --pred-file "$2" --exact-match ;
+else
+  python eval.py --gold-file temp_out.jsonl --pred-file "$2"
+fi
 rm temp_out.jsonl
