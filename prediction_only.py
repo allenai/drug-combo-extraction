110da4be3aa5ffe756565da19ec05323cd6ef894
Vijay Viswanathan <vcomeinpeace@gmail.com>
2021-08-19 12:42:33 -0400

    Rename modified test_only.py -> prediction_only.py

--- prediction_only.py
+++ prediction_only.py
@@ -14,12 +14,11 @@ from utils import construct_row_id_idx_mapping, set_seed, write_error_analysis_f
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--checkpoint-path', type=str, required=False, default="checkpoints", help="Path to pretrained Huggingface Transformers model")
-parser.add_argument('--test-file', type=str, required=False, default="data/dev_set_error_analysis.jsonl")
+parser.add_argument('--test-file', type=str, required=False, default="data/dev_set_error_analysis.jsonl", help="This should be a test file, with labels concealed (to ensure no label leakage when making predictions).")
 parser.add_argument('--batch-size', type=int, default=32, help="Batch size for testing (larger batch -> faster evaluation)")
 parser.add_argument('--outputs-directory', type=str, required=False, help="Output directory where we write predictions, for offline evaluation", default="/tmp/outputs/.tsv")
 parser.add_argument('--error-analysis-file', type=str, required=False, help="Output file containing error analysis information", default="test_output.tsv")
 parser.add_argument('--seed', type=int, required=False, default=2021)
-parser.add_argument('--produce_all_subsets', action='store_true', help="If true, and we are including no-comb relations, then include all subsets of existing relations as NO_COMB as well")
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -35,7 +34,8 @@ if __name__ == "__main__":
                                only_include_binary_no_comb_relations=metadata.only_include_binary_no_comb_relations,
                                include_paragraph_context=metadata.include_paragraph_context,
                                context_window_size=metadata.context_window_size,
-                               produce_all_subsets=args.produce_all_subsets)
+                               produce_all_subsets=True,
+                               hide_labels=True)
     row_id_idx_mapping, idx_row_id_mapping = construct_row_id_idx_mapping(test_data)
     dm = DrugSynergyDataModule(None,
                                test_data,
@@ -46,10 +46,11 @@ if __name__ == "__main__":
                                dev_batch_size=args.batch_size,
                                test_batch_size=args.batch_size,
                                max_seq_length=metadata.max_seq_length,
-                               balance_training_batch_labels=False)
+                               balance_training_batch_labels=False,
+                               hide_test_labels=True)
     dm.setup()
 
-    system = RelationExtractor(model, 0, tokenizer=tokenizer)
+    system = RelationExtractor(model, 0, tokenizer=tokenizer, test_labels_hidden=True)
     trainer = pl.Trainer(
         gpus=1,
         precision=16,
@@ -65,4 +66,4 @@ if __name__ == "__main__":
     test_output = os.path.join(args.outputs_directory, "predictions.jsonl")
 
     write_jsonl(fixed_test, test_output)
-    write_error_analysis_file(test_data, test_data_raw, test_row_ids, test_predictions, args.error_analysis_file)
+    write_error_analysis_file(test_data, test_data_raw, test_row_ids, test_predictions, args.error_analysis_file, hide_labels=True)
