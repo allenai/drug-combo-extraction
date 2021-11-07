e08e69fda06545058cfd8e33e0ad3d545792ec73
Vijay Viswanathan <vcomeinpeace@gmail.com>
2021-11-07 16:33:26 +0000

    Add the repo root to path

--- scripts/train.py
+++ scripts/train.py
@@ -14,6 +14,7 @@ from transformers import AutoTokenizer
 from transformers.file_utils import PYTORCH_PRETRAINED_BERT_CACHE
 
 import sys
+sys.path.append('.')
 sys.path.append('..')
 from common.constants import ENTITY_END_MARKER, ENTITY_START_MARKER, NOT_COMB
 from preprocessing.data_loader import DrugSynergyDataModule
